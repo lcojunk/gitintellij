803b93c6400cff4e9fe9da9e3a103caee3e36c77
lcojunk <lcojunk@gmail.com>
2017-08-23 11:34:39 +0200

    Added version and start-time parameters

--- src/main/java/leo/example/gitIntelij/GitIntelijApplication.java
+++ src/main/java/leo/example/gitIntelij/GitIntelijApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitIntelijApplication {
-
+	public static final String DEVELOPMENT_VERSION = "0.1.12";
+	public static final long APPLICATION_START_TIME = System.currentTimeMillis();
 	public static void main(String[] args) {
 		SpringApplication.run(GitIntelijApplication.class, args);
 	}
