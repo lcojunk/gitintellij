bda95d27037971c635c564d37c74592534298541
Odzhara-Ongom <Leon.Odzhara-Ongom@adesso.de>
2017-08-23 11:21:31 +0200

    First commit

---
+++ src/main/java/leo/example/gitIntelij/GitIntelijApplication.java
@@ -0,0 +1,12 @@
+package leo.example.gitIntelij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitIntelijApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitIntelijApplication.class, args);
+	}
+}
